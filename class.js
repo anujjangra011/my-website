121c81afd49cb442b65e850cb3f65bd94bcf3e42
anujjangra011 <146470410+anujjangra011@users.noreply.github.com>
2024-09-23 00:37:59 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,8 @@
+<script>
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+</script>
